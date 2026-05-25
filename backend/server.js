684a643a7fdc63cff55aa92c53312da5b1e7da84
Joao <garofalojoaoneto@gmail.com>
2026-05-25 14:41:57 -0300

    Atualização

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const SECRET = "estoque_inteligente";
+
 app.get("/", (req, res) => {
   res.send("API Estoque Inteligente funcionando");
 });
@@ -94,6 +96,28 @@ app.listen(3000, () => {
   console.log("Servidor rodando");
 });
 
+app.get("/me", (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({
+      mensagem: "Token não enviado",
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, SECRET);
+
+    res.json(decoded);
+  } catch {
+    res.status(401).json({
+      mensagem: "Token inválido",
+    });
+  }
+});
+
 app.post("/produtos", (req, res) => {
   const { nome, quantidade, preco } = req.body;
 
@@ -109,7 +133,9 @@ app.post("/produtos", (req, res) => {
       res.json({
         id: this.lastID,
         nome,
-        quantidade,
+        codigo,
+        categoria,
+        estoque,
         preco,
       });
     }
@@ -122,6 +148,132 @@ app.get("/produtos", (req, res) => {
       return res.status(500).json(err);
     }
 
-    res.json(rows);
+    res.json({
+      messagem: "produto criado com sucesso",
+      id: this.lastID,
+    });
   });
 });
+
+app.get("/produtos", (req, res) => {
+  db.all(
+    `SELECT * FROM produtos`,
+    [],
+    (err, produtos) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+
+      res.json(produtos);
+    }
+  );
+});
+
+app.get("/produtos/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.get(
+    `SELECT * FROM produtos WHERE id = ?`,
+    [id],
+    (err, produto) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+
+      res.json(produto);
+    }
+  );
+});
+
+app.put("/produtos/:id", (req, res) => {
+  const { id } = req.params;
+
+  const {
+    nome,
+    codigo,
+    categoria,
+    preco,
+    estoque,
+  } = req.body;
+
+  db.run(
+    `
+    UPDATE produtos
+    SET
+      nome = ?,
+      codigo = ?,
+      categoria = ?,
+      preco = ?,
+      estoque = ?
+    WHERE id = ?
+    `,
+    [
+      nome,
+      codigo,
+      categoria,
+      preco,
+      estoque,
+      id,
+    ],
+    function (err) {
+      if (err) {
+        return res.status(500).json(err);
+      }
+
+      res.json({
+        mensagem: "Produto atualizado",
+      });
+    }
+  );
+});
+
+app.delete("/produtos/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.run(
+    `DELETE FROM produtos WHERE id = ?`,
+    [id],
+    function (err) {
+      if (err) {
+        return res.status(500).json(err);
+      }
+
+      res.json({
+        mensagem: "Produto deletado",
+      });
+    }
+  );
+});
+
+db.run(`
+CREATE TABLE IF NOT EXISTS usuarios (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  email TEXT,
+  senha TEXT,
+  nivel TEXT
+)
+`);
+
+db.run(`
+CREATE TABLE IF NOT EXISTS produtos (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  nome TEXT,
+  codigo TEXT,
+  categoria TEXT,
+  preco REAL,
+  estoque INTEGER,
+  validade TEXT,
+  lote TEXT,
+  foto TEXT
+)
+`);
+
+db.run(`
+CREATE TABLE IF NOT EXISTS vendas (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  produto_id INTEGER,
+  quantidade INTEGER,
+  valor REAL,
+  data TEXT
+)
+`);
